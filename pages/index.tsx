ac9d885275ea4b253b1c06799dd945cad23a55d8
Haru0101 <haruki.for.01@gmail.com>
2022-04-21 09:06:21 +0900

    feat: add item function

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,18 +6,29 @@ const Home: NextPage = () => {
   const listItems = items.map((item, index) => {
     return <li key={index}>{item}</li>;
   });
+  const [newItem, setNewItem] = useState("");
+
+  const addItems = () => {
+    setItems([...items, newItem]);
+    setNewItem("");
+  }
+
+  const submitForm = (e: { preventDefault: () => void; }) => {
+    e.preventDefault();
+  }
+
   return (
     <div>
       <h1>人生でやりたい100のこと</h1>
       <ol>
         {listItems}
       </ol>
-      <form>
+      <form onSubmit={submitForm}>
         <label>
           やりたいこと：
-          <input type="text" />
+          <input type="text" onChange={e => setNewItem(e.target.value)} value={newItem}/>
         </label>
-        <button>追加</button>
+        <button onClick={addItems}>追加</button>
       </form>
     </div>
   )
