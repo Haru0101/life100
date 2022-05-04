d1877a8fcd58bb0f0bccd1316b6e652d496b4d0c
Haru0101 <haruki.for.01@gmail.com>
2022-05-04 17:47:11 +0900

    fix: margin

--- pages/index.tsx
+++ pages/index.tsx
@@ -14,7 +14,7 @@ const Home: NextPage = () => {
   });
 
   const listItems = items?.map((item, index) => {
-    return <li className='w-1/2 flex items-center' key={index}><input type='checkbox' checked={item.isDone} onChange={e => completeItem(index, e.target.checked)} name='' id='' /><span className={item.isDone ? 'line-through' : ''}>{item.content}</span><button onClick={() => deleteItem(index)}><svg xmlns="http://www.w3.org/2000/svg" className="ml-1 h-4 w-4 text-slate-400" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}>
+    return <li className='w-1/2 flex items-center' key={index}><input type='checkbox' checked={item.isDone} onChange={e => completeItem(index, e.target.checked)} name='' id='' className='mr-1'/><span className={item.isDone ? 'line-through' : ''}>{item.content}</span><button onClick={() => deleteItem(index)}><svg xmlns="http://www.w3.org/2000/svg" className="ml-1 h-4 w-4 text-slate-400" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}>
     <path strokeLinecap="round" strokeLinejoin="round" d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z" />
   </svg></button></li>;
   });
